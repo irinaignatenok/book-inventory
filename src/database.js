91cd503ec052e7532b2be3b61c4c922351d09cd1
irinaignatenok <64011679+irinaignatenok@users.noreply.github.com>
2025-01-26 13:24:54 -0500

    rebuild sqllite3

--- src/database.js
+++ src/database.js
@@ -29,37 +29,30 @@
 
 // module.exports = db;
 
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('better-sqlite3');
 
 // Set the database file path based on the environment
 const dbFilePath = process.env.DATABASE_URL || './book-inventory.db';
 
 // Create a new database instance
-const db = new sqlite3.Database(dbFilePath, (err) => {
-    if (err) {
-        console.error('Error opening database:', err.message);
-    } else {
-        console.log('Connected to the SQLite database.');
-        // Inventory table 
+const db = new sqlite3(dbFilePath, { verbose: console.log });
 
-        db.run(
-            `CREATE TABLE IF NOT EXISTS Inventory (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                title TEXT NOT NULL,
-                author TEXT NOT NULL,
-                genre TEXT NOT NULL,
-                publicationDate TEXT NOT NULL,
-                isbn TEXT NOT NULL UNIQUE
-            )`,
-            (err) => {
-                if (err) {
-                    console.error('Error creating table:', err.message);
-                } else {
-                    console.log('Inventory table created.');
-                }
-            }
-        );
-    }
-});
+try {
+    // Create the Inventory table if it doesn't exist
+    db.prepare(`
+        CREATE TABLE IF NOT EXISTS Inventory (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            author TEXT NOT NULL,
+            genre TEXT NOT NULL,
+            publicationDate TEXT NOT NULL,
+            isbn TEXT NOT NULL UNIQUE
+        )
+    `).run();
+    console.log('Inventory table created.');
+} catch (err) {
+    console.error('Error creating table:', err.message);
+}
 
 module.exports = db;
+
