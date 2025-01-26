8be946c59a378546153ccb909f451ef72b335787
irinaignatenok <64011679+irinaignatenok@users.noreply.github.com>
2025-01-26 13:18:42 -0500

    reinstall sqllite

--- src/database.js
+++ src/database.js
@@ -1,6 +1,41 @@
+// const sqlite3 = require('sqlite3').verbose();
+// // Create a new database instance
+// const db = new sqlite3.Database('./book-inventory.db', (err) => {
+//     if (err) {
+//         console.error('Error opening database:', err.message);
+//     } else {
+//         console.log('Connected to the SQLite database.');
+//         // Inventory table 
+
+//         db.run(
+//             `CREATE TABLE IF NOT EXISTS Inventory (
+//       id INTEGER PRIMARY KEY AUTOINCREMENT,
+//       title TEXT NOT NULL,
+//       author TEXT NOT NULL,
+//       genre TEXT NOT NULL,
+//       publicationDate TEXT NOT NULL,
+//       isbn TEXT NOT NULL UNIQUE
+//     )`,
+//             (err) => {
+//                 if (err) {
+//                     console.error('Error creating table:', err.message);
+//                 } else {
+//                     console.log('Inventory table created.');
+//                 }
+//             }
+//         );
+//     }
+// });
+
+// module.exports = db;
+
 const sqlite3 = require('sqlite3').verbose();
+
+// Set the database file path based on the environment
+const dbFilePath = process.env.DATABASE_URL || './book-inventory.db';
+
 // Create a new database instance
-const db = new sqlite3.Database('./book-inventory.db', (err) => {
+const db = new sqlite3.Database(dbFilePath, (err) => {
     if (err) {
         console.error('Error opening database:', err.message);
     } else {
@@ -9,13 +44,13 @@ const db = new sqlite3.Database('./book-inventory.db', (err) => {
 
         db.run(
             `CREATE TABLE IF NOT EXISTS Inventory (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT NOT NULL,
-      author TEXT NOT NULL,
-      genre TEXT NOT NULL,
-      publicationDate TEXT NOT NULL,
-      isbn TEXT NOT NULL UNIQUE
-    )`,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                author TEXT NOT NULL,
+                genre TEXT NOT NULL,
+                publicationDate TEXT NOT NULL,
+                isbn TEXT NOT NULL UNIQUE
+            )`,
             (err) => {
                 if (err) {
                     console.error('Error creating table:', err.message);
